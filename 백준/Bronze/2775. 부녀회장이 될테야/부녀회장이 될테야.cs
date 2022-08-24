6bee66ba0fdcc7432c9ef7ecd645fd8e2690c22a
mynameisjinhohong <110793489+mynameisjinhohong@users.noreply.github.com>
2022-08-24 16:08:21 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 72 ms, Memory: 5808 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[15, 14];
+            for(int i =0; i<14; i++)
+            {
+                a[0, i] = i+1;
+            }
+            for(int i =0; i<15; i++)
+            {
+                a[i, 0] = 1;
+            }
+            for(int i =1; i<15; i++)
+            {
+                for(int j = 1; j<14; j++)
+                {
+                    a[i, j] = a[i - 1, j] + a[i, j - 1];
+                    //Console.Write(a[i, j] + " ");
+                }
+                //Console.Write("\n");
+            }
+
+            //for (int i = 0; i < 15; i++)
+            //{
+            //    for (int j = 0; j < 14; j++)
+            //    {
+            //        Console.Write(a[i, j] + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            int b = int.Parse(Console.ReadLine());
+            int[] e = new int[b];
+            for (int i = 0; i < b; i++)
+            {
+                int c = int.Parse(Console.ReadLine());
+                int d = int.Parse(Console.ReadLine());
+                e[i] = a[c, d - 1];
+            }
+            for (int i = 0; i < e.Length; i++)
+            {
+                Console.WriteLine(e[i]);
+            }
+        }
+
+    }
+}
