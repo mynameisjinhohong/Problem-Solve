6a25bcc131a8e81ebf0389879ece3e7d802d34cd
JinHo Hong <110793489+mynameisjinhohong@users.noreply.github.com>
2022-11-15 09:52:43 +0900

    [Bronze V] Title: 제리와 톰, Time: 68 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Numerics;
+using System.Diagnostics;
+namespace TEST
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a = Console.ReadLine().Split();
+            int b = int.Parse(a[0]);
+            int c = int.Parse(a[1]);
+            Console.Write(c - b + " " + c);
+
+        }
+        
+    }
+
+}
