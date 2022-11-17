16168829fe3e60801c4f1c0e28e4e8ea0d3c76ab
JinHo Hong <110793489+mynameisjinhohong@users.noreply.github.com>
2022-11-18 08:59:35 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 2032 ms, Memory: 6596 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Numerics;
+using System.Diagnostics;
+namespace TEST
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            for(int i =0; i< a ; i++)
+            {
+                for(int j =0; j<i+1; j++)
+                {
+                    Console.Write("*");
+                }
+                for(int j = 0; j<a*2 - (i+1)*2; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            for(int i =a-1; i>0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                for (int j = 0; j < a * 2 - (i * 2); j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+        }
+        
+    }
+
+}
