cb4ceabd9b8ddeaa0f3e11e21d6432c062047e21
JinHo Hong <110793489+mynameisjinhohong@users.noreply.github.com>
2022-12-24 11:33:37 +0900

    [Bronze V] Title: Hello World, Time: 48 ms, Memory: 5320 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.cs
+++ 백준/Bronze/2557. Hello World/Hello World.cs
@@ -1,17 +1,1 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.IO;
-using System.Numerics;
-using System.Diagnostics;
-namespace TEST
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+Console.Write("Hello World!");
