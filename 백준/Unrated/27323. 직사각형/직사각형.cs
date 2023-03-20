8729f8d4443c15d25cef7a458d6e0d45f7f999e9
JinHo Hong <110793489+mynameisjinhohong@users.noreply.github.com>
2023-03-20 10:21:51 +0900

    [Unrated] Title: 직사각형, Time: 64 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Unrated/27323. 직사각형/직사각형.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a*b);
+        }
+
+    }
+}
