460a4b0e31c413443a4385fdfddad814f3edd94e
JinHo Hong <110793489+mynameisjinhohong@users.noreply.github.com>
2023-04-17 10:39:17 +0900

    [Bronze IV] Title: 한글 2, Time: 64 ms, Memory: 5448 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                char[] a = Console.ReadLine().ToCharArray();
+                Console.WriteLine((int)a[0] - 44031);
+        }
+
+
+    }
+}
+    
