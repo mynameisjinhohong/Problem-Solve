ba355719d12c7c686538206ae5d0dbf1aee139a3
JinHo Hong <110793489+mynameisjinhohong@users.noreply.github.com>
2023-05-11 08:47:19 +0900

    [Bronze V] Title: SciComLove, Time: 76 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            for(int i =0; i < a; i++)
+            {
+                Console.WriteLine("SciComLove");
+            }
+        }
+    }
+}
+    
