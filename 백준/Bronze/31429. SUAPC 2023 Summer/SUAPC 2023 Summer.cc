6da443de986d5b21c9f6ecc93aba49e745b1f726
JinHo Hong <ghddhksduq@naver.com>
2024-10-09 16:09:11 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+
+int main()
+{
+    int a = 0;
+    std::cin >> a;
+	switch (a)
+	{
+	case 1:
+		std::cout << "12 1600" << std::endl;
+		break;
+	case 2:
+		std::cout << "11 894" << std::endl;
+		break;
+	case 3:
+		std::cout << "11 1327" << std::endl;
+		break;
+	case 4:
+		std::cout << "10 1311" << std::endl;
+		break;
+	case 5:
+		std::cout << "9 1004" << std::endl;
+		break;
+	case 6:
+		std::cout << "9 1178" << std::endl;
+		break;
+	case 7:
+		std::cout << "9 1357" << std::endl;
+		break;
+	case 8:
+		std::cout << "8 837" << std::endl;
+		break;
+	case 9:
+		std::cout << "7 1055" << std::endl;
+		break;
+	case 10:
+		std::cout << "6 556" << std::endl;
+		break;
+	case 11:
+		std::cout << "6 773" << std::endl;
+		break;
+	}
+	return 0;	
+}
