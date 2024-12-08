3b87811dcb41394d6388e1a294298dbc88d7d06f
JinHo Hong <ghddhksduq@naver.com>
2024-12-08 11:17:37 +0900

    [Bronze V] Title: 가희와 4시간의 벽 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32775. 가희와 4시간의 벽 1/가희와 4시간의 벽 1.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+	int a;
+	int b;
+	cin >> a >> b;
+	if (a <= b) {
+		cout << "high speed rail";
+	}
+	else if (a > b) {
+		cout << "flight";
+	}
+
+	
+}
