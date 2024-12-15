fd818ddcba0845397c2ac7dc03964f6396144bb5
JinHo Hong <ghddhksduq@naver.com>
2024-12-15 09:58:25 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+	string a;
+	cin >> a;
+	if (a == "N" || a == "n") {
+		cout << "Naver D2" << endl;
+	}
+	else {
+		cout << "Naver Whale" << endl;
+	}
+}
