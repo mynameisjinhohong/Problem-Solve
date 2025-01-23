79c579aa49688186a95aeea601f0c01a6df3c1a0
JinHo Hong <ghddhksduq@naver.com>
2025-01-23 11:56:31 +0900

    [Bronze IV] Title: ISBN, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6810. ISBN/ISBN.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout << "The 1-3-sum is " << 91 + a + b * 3 + c;
+	
+}
