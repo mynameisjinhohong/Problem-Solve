e4b3cbe0477031ebf08858952fac93bc9b850679
JinHo Hong <ghddhksduq@naver.com>
2025-01-26 11:11:01 +0900

    [Bronze III] Title: 수박 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31868. 수박 게임/수박 게임.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+	for (int i = 1; i < n; i++) {
+		k = k / 2;
+	}
+	cout << k;
+	
+}
