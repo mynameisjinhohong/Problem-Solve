df8b1af09a71f7385e61131923c8633fbab12ef9
JinHo Hong <ghddhksduq@naver.com>
2025-02-12 12:52:29 +0900

    [Bronze IV] Title: Population, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26561. Population/Population.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int a, b;
+		cin >> a >> b;
+		cout << a + (b / 4) - (b / 7) << endl;
+	}
+
+	
+}
