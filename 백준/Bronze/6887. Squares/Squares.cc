0be1e59a2c0dc8ec7d6559e8d4dec0e10d8b4daf
JinHo Hong <ghddhksduq@naver.com>
2025-02-26 11:49:38 +0900

    [Bronze IV] Title: Squares, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/6887. Squares/Squares.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	cin >> a;
+	cout <<"The largest square has side length " << floor(sqrt(a)) << "." << endl;
+}
