da9388f87245728e14d56a677bc5acee070435fb
JinHo Hong <ghddhksduq@naver.com>
2025-03-12 12:21:20 +0900

    [Bronze IV] Title: No Brainer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	cin >> a;
+	for (int i = 0; i < a; i++) {
+		int b,c;
+		cin >> b >> c;
+		if (c > b) {
+			cout << "NO BRAINS" << endl;
+		}
+		else {
+			cout << "MMM BRAINS" << endl;
+		}
+	}
+
+}
