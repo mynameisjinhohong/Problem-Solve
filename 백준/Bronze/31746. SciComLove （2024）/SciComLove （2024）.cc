fdc7dedaa0ab1bf2de244aadf5ccc74aff52c2b4
JinHo Hong <ghddhksduq@naver.com>
2025-03-24 11:06:41 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <vector>
+#include <list>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	cin >> a;
+	if (a % 2 == 0)
+	{
+		cout << "SciComLove";
+	}
+	else
+	{
+		cout << "evoLmoCicS";
+	}
+}
