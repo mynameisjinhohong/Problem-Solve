ab184bb0ecf8b3f97fccced908565ad35476fd27
JinHo Hong <ghddhksduq@naver.com>
2025-05-04 13:20:21 +0900

    [Bronze V] Title: 母音を数える (Counting Vowels), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/18409. 母音を数える （Counting Vowels）/母音を数える （Counting Vowels）.py
@@ -0,0 +1,15 @@
+a = int(input())
+b = input()
+ans = 0
+for i in b:
+    if i == 'a':
+        ans += 1
+    elif i == 'i':
+        ans += 1
+    elif i =='u':
+        ans += 1
+    elif i == 'e':
+        ans += 1
+    elif i == 'o':
+        ans += 1
+print(ans)
