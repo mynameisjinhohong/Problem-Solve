a46d404833e71f1632b5b5778dbe7b6c885f3914
JinHo Hong <ghddhksduq@naver.com>
2025-05-18 12:13:26 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <vector>
+#include <list>
+#include <map>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	string a;
+	while (getline(cin, a)) {
+		cout << a << endl;
+	}
+}
