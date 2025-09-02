96a8f8a3a2dd6c1453ed59fb2b2b80cf68122386
JinHo Hong <ghddhksduq@naver.com>
2025-09-03 08:57:36 +0900

    [Bronze IV] Title: 주사위, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    for(int i =0; i<a; i++){
+        int b,c;
+        cin >> b >> c;
+        cout << "Case " << i+1 <<": " << b+c << "\n";
+    }
+    
+    return 0;
+}
