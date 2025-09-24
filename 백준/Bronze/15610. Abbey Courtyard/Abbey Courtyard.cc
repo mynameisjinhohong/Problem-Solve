2ce795464ff0278cbbb2431d81be28696f41f008
JinHo Hong <ghddhksduq@naver.com>
2025-09-24 10:12:36 +0900

    [Bronze IV] Title: Abbey Courtyard, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/15610. Abbey Courtyard/Abbey Courtyard.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    double a;
+    cin >> a;
+    cout << fixed;
+    cout.precision(8);
+    cout << sqrt(a) * 4;
+    return 0;
+}
