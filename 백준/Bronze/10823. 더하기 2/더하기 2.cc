8cff43521deead0ec4ef396c67057d70e82553e2
JinHo Hong <ghddhksduq@naver.com>
2025-09-29 09:32:30 +0900

    [Bronze I] Title: 더하기 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10823. 더하기 2/더하기 2.cc
+++ 백준/Bronze/10823. 더하기 2/더하기 2.cc
@@ -1,26 +1,24 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    string real, line;
-    while (getline(cin, line)) {   // EOF까지 한 줄씩 읽기
-        real += line;              // 줄바꿈은 무시하고 붙이기
-    }
-
+    string a;
+    string real;
     long long ans = 0;
-    size_t beforeIdx = 0;
-    size_t idx;
-
-    while ((idx = real.find(',', beforeIdx)) != string::npos) {
-        ans += stol(real.substr(beforeIdx, idx - beforeIdx));
+    while (cin >> a) real += a;
+    int beforeIdx = 0;
+    int idx = 0;
+    while(true){
+        idx = real.find(',',beforeIdx);
+        if(idx == string::npos){
+            break;
+        }
+        ans += stol(real.substr(beforeIdx,idx-beforeIdx));
         beforeIdx = idx + 1;
     }
-
-    ans += stol(real.substr(beforeIdx));
+    ans += stol(real.substr(beforeIdx,real.size() - beforeIdx));
     cout << ans;
     return 0;
 }
