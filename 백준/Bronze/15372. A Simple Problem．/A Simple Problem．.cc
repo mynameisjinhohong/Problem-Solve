b831e0debeddc39c8a1c8bb2d7cf87dede3b5e18
JinHo Hong <ghddhksduq@naver.com>
2025-11-08 10:40:02 +0900

    [Bronze IV] Title: A Simple Problem., Time: 36 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15372. A Simple Problem．/A Simple Problem．.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <stdlib.h>
+#include <unordered_map>
+#include <cmath>
+
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    std::cout.tie(NULL);
+    int a;
+    cin >> a;
+    for (int i = 0; i < a; i++) {
+        long long b;
+        cin >> b;
+		cout << b * b << "\n";
+
+    }
+}
+
