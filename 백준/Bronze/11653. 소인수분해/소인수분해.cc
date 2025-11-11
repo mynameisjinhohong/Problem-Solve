7f3de21394f54ef242a03f388884588baa7ff573
JinHo Hong <ghddhksduq@naver.com>
2025-11-11 09:03:32 +0900

    [Bronze I] Title: 소인수분해, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    while(a != 1){
+        for(int i = 2; i<=a; i++){
+            if(a%i == 0){
+                a /= i;
+                cout << i << "\n";
+                break;
+            }
+        }        
+    }
+
+    return 0;
+}
