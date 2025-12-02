c63aca8d01bc2e50563cda8edc39f083c376cae8
JinHo Hong <ghddhksduq@naver.com>
2025-12-02 09:08:44 +0900

    [Bronze IV] Title: Counting Clauses, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17903. Counting Clauses/Counting Clauses.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    if(a >= 8){
+        cout << "satisfactory";
+    }
+    else{
+        cout << "unsatisfactory";
+    }
+    return 0;
+}
