0082c54db7860f3ae9643c02bd3c3fa077d0a64c
JinHo Hong <ghddhksduq@naver.com>
2026-02-22 18:02:11 +0900

    [Bronze I] Title: 비숍 투어, Time: 44 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/23885. 비숍 투어/비숍 투어.py
+++ 백준/Bronze/23885. 비숍 투어/비숍 투어.py
@@ -1,7 +1,13 @@
 a,b = map(int,input().split())
 c,d = map(int,input().split())
 e,f = map(int,input().split())
-if (c+d)%2 == (e+f)%2:
-    print("YES")
+if a == 1 or b == 1:
+    if c == e and d == f:
+        print("YES")
+    else:
+        print("NO")
 else:
-    print("NO")
+    if (c+d)%2 == (e+f)%2:
+        print("YES")
+    else:
+        print("NO")
