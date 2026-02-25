cdbb4ccf74f6318bbdcf8bbff4c0161c1b52c01e
JinHo Hong <ghddhksduq@naver.com>
2026-02-25 09:04:47 +0900

    [Bronze IV] Title: Zero or One, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13623. Zero or One/Zero or One.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    if(a==b && b == c){
+        cout << "*";
+    }
+    else if(a == b){
+        cout << "C";
+    }
+    else if(a == c){
+        cout << "B";
+    }
+    else{
+        cout << "A";
+    }
+    return 0;
+}
