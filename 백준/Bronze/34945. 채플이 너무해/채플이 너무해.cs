1c783566c2ff4b1bcd13509973541704a5578331
JinHo Hong <ghddhksduq@naver.com>
2026-03-11 09:14:43 +0900

    [Bronze V] Title: 채플이 너무해, Time: 48 ms, Memory: 5460 KB -BaekjoonHub

---
+++ 백준/Bronze/34945. 채플이 너무해/채플이 너무해.cs
@@ -0,0 +1,7 @@
+            int a = int.Parse(Console.ReadLine());
+            if(a>5){
+                Console.WriteLine("Success!");
+            }
+            else{
+                Console.WriteLine("Oh My God!");
+            }
